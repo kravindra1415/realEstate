13a836e03fa3be2182d22b88b119240edeaf2154
kravindra1415 <kravindra1415@gmail.com>
2022-11-08 15:27:37 +0530

    retry & retryWhen

--- src/app/service/httpError-interceptor.service.ts
+++ src/app/service/httpError-interceptor.service.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { catchError, Observable, throwError } from "rxjs";
+import { catchError, concatMap, Observable, of, retry, retryWhen, throwError } from "rxjs";
+import { ErrorCode } from "../enums/enum";
 import { AlertifyService } from "./alertify.service";
 
 @Injectable({
@@ -14,6 +15,8 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
         console.log('Http request Started..');
         return next.handle(request)
             .pipe(
+                // retry(10), => it try to login 10 times the api/method in case of wrong attempts. 
+                retryWhen(error => this.retryRequest(error, 10)),
                 catchError((error: HttpErrorResponse) => {
                     const errorMeassage = this.setError(error)
                     console.log(error);
@@ -21,6 +24,36 @@ export class HttpErrorInterceptorService implements HttpInterceptor {
                     return throwError(errorMeassage);
                 })
             );
+    }
+
+    //retry the request in case of error
+    retryRequest(error: Observable<any>, retryCount: number): Observable<unknown> {
+        return error.pipe(
+            concatMap((checkErr: HttpErrorResponse, count: number) => {
+                //  retry in case WEB API is down
+                // if (checkErr.status === ErrorCode.serverDown && count <= retryCount) {
+                //     return of(checkErr);
+                // }
+
+                // //retry in case unauthorized error
+                // if (checkErr.status === ErrorCode.unauthorized && count <= retryCount) {
+                //     return of(checkErr);
+                // }
+
+                if (count <= retryCount) {
+                    switch (checkErr.status) {
+                        case ErrorCode.serverDown:
+                            return of(checkErr);
+
+                        case ErrorCode.unauthorized:
+                            return of(checkErr);
+
+                    }
+                }
+
+                return throwError(checkErr);
+            })
+        )
     }
 
     setError(error: HttpErrorResponse): string {
